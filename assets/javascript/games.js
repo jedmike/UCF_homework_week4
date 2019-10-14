f9e072802dc5f9b01ea545fef64fb95ad12fe622
Mike Jed <jedmike@gmail.com>
2019-10-14 17:13:10 -0400

    add .js file and udated index file

---
+++ assets/javascript/games.js
@@ -0,0 +1,62 @@
+alert("test");
+console.log (purpleC);
+// Global Variables
+    //Crystal Tracking-got this from solution video would have done seperatly
+    var crystal = {
+            purple: {
+                name: "amethyst",
+                value: 0
+            },
+            red:    {
+                name: "ruby",
+                value: 0
+            },       
+            blue:   {
+                name: "saphire",
+                value: 0
+            },
+            green:  {
+                name: "emerald",
+                value: 0
+            }
+    };
+        //Track scoring
+    let your_score  = 0;
+    let your_target = 0;
+    let wins        = 0;
+    let losses      = 0;
+
+// ____________________________________________________________________
+
+// Functions
+
+// _____________________________________________________________________
+
+// Process logic
+
+// ______________________________________________________________________
+    // Click event on crystals
+$("#purpleC").click = (function() {
+    console.log ("works");
+});
+
+// start_function
+        // Counters reset to zero (wins, losses)
+//  game_loop function
+        // Counters reset to zero (target, actual)
+
+//  Random generator between 19-120
+        //set target 
+//  Random generator between 1-12
+        //set for purple
+        //set for red
+        //set for blue
+        //set for green
+//  Player clcks a crystal, 
+        //add to actual
+        //check actual against target, if equal win++ "you Win"
+        //check if > target, if true, loses ++ "you loose"
+        //check if < target, if true, "click again"
+//  game_loop
+//  
+
